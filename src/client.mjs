025465e4d290482bb311b407a3f61f70024be92f
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-05-28 20:58:30 +0200

    gyro settings

--- src/client.mjs
+++ src/client.mjs
@@ -1,23 +1,18 @@
-import Notification from '@uncut/gyro/components/Notification.js';
-import '@uncut/gyro/css/gyro.css';
 import '@uncut/gyro-layout';
-import '@uncut/gyro/components/menu-bar/Menubar';
-import '@uncut/gyro/components/Icon.js';
-import '@uncut/gyro/components/Knob.js';
 import '@uncut/gyro/components/FluidInput.js';
-import '@uncut/gyro/components/LevelSlider.js';
+import '@uncut/gyro/components/Icon.js';
 import '@uncut/gyro/components/Input.js';
+import '@uncut/gyro/components/Knob.js';
+import '@uncut/gyro/components/LevelSlider.js';
+import '@uncut/gyro/components/menu-bar/Menubar';
+import { SettingsComponent } from '@uncut/gyro/components/settings/Settings.js';
+import '@uncut/gyro/css/gyro.css';
+import { Action } from '@uncut/gyro/src/core/Actions';
 import '../components/emote-editor/EmoteEditor.js';
-
-window.log = (...str) => {
-    new Notification({ text: str.join(" "), time: 2000 }).show();
-    console.log(...str);
-}
 
 window.addEventListener('DOMContentLoaded', init());
 
 function updatePreviews(croppedImage) {
-
     if(!croppedImage) return;
 
     const canvases = document.querySelectorAll('.emote-preview');
@@ -27,38 +22,42 @@ function updatePreviews(croppedImage) {
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.drawImage(croppedImage, 0, 0, canvas.width, canvas.height);
     }
-
 }
 
-window.addEventListener('dragover', e => {
-    e.preventDefault();
-});
-
-window.addEventListener('drop', e => {
-    if(e.dataTransfer.items) {
-        for(let item of e.dataTransfer.items) {
-            if (item.kind === 'file') {
-                const file = item.getAsFile();
-
-                const reader  = new FileReader();
-                reader.onload = function(e)  {
-                    const img = new Image();
-                    img.src = e.target.result;
-
-                    img.onload = () => {
-                        const editor = document.querySelector('gyro-emote-editor');
-                        editor.viewAsset({ data: img });
-
-                        updatePreviews(editor.getPreviewOutput());
+function enableFileDragAndDrop() {
+    
+    window.addEventListener('dragover', e => {
+        e.preventDefault();
+    });
+    
+    window.addEventListener('drop', e => {
+        if(e.dataTransfer.items) {
+            for(let item of e.dataTransfer.items) {
+                if (item.kind === 'file') {
+                    const file = item.getAsFile();
+    
+                    const reader  = new FileReader();
+                    reader.onload = function(e)  {
+                        const img = new Image();
+                        img.src = e.target.result;
+    
+                        img.onload = () => {
+                            const editor = document.querySelector('gyro-emote-editor');
+                            editor.viewAsset({ data: img });
+    
+                            updatePreviews(editor.getPreviewOutput());
+                        }
                     }
+                    reader.readAsDataURL(file);
                 }
-                reader.readAsDataURL(file);
             }
         }
-    }
-})
+    })
+}
 
 async function init() {
+    enableFileDragAndDrop();
+
     const editor = document.querySelector('gyro-emote-editor');
 
     editor.addEventListener('contextmenu', e => {
